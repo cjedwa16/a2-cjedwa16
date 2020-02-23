df10632022eaf818ef03782ba00ac53b824213a7
Cameron Edwards <cjedwa16@stlawu.edu>
2020-02-23 15:59:59 -0500

    test

--- query.js
+++ query.js
@@ -31,7 +31,7 @@ Voter.distinct('zipCode')
 Promise.all(queries)
   .then(function(results) {
     console.log('The number of registered voters that live in zip code 13617: ', results[0]);
-    console.log('The full names of all of the registered voters whose first-name is STARR: ', results[1].map(v => v.firstName + v.lastName));
+    console.log('The full names of all of the registered voters whose first-name is STARR: ', results[1].map(v => v.firstName + ' ' + v.lastName));
     console.log('Number of people who voted in the GE 2016: ', results[2]);
     console.log('The last-name that comes in last in the county in alphabetical order:  ', results[3].map(v => v.lastName));
     console.log('The number of zip codes that the county contains: ', results[4].length);
