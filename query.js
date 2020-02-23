fec69199731704178dfd4085bf2477a4865c592c
Cameron Edwards <cjedwa16@stlawu.edu>
2020-02-23 13:52:27 -0500

    forgot closing bracket

--- query.js
+++ query.js
@@ -30,7 +30,7 @@ Voter.distinct('zipCode')
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(results) {
-    console.log('Number of registered voters that live in zip code 13617 ', results[0);
+    console.log('Number of registered voters that live in zip code 13617 ', results[0]);
     console.log('The full names of all of the registered voters whose first name is STARR: ', results[1].map(p => p.firstName + p.lastName));
     console.log('Number of people who voted in the GE 2016: ', results[2]);
     console.log('last-name that comes in last in the county in alphabetical order:  ', results[3].map(p => p.lastName));
