2c0b8894878f04e018d6e991736d129d8e6075d5
Cameron Edwards <cjedwa16@stlawu.edu>
2020-02-23 15:46:37 -0500

    updates

--- query.js
+++ query.js
@@ -10,15 +10,15 @@ connect(); // To the database
 const queries = [
 
 // How many registered voters live in the Canton zip code (13617)?
-Voter.find().where('zipCode').equals(13617),
+Voter.find().where('zipCode').equals('13617'),
 
 // What are the full names of all the registered voters whose first-name is STARR?
 Voter.find().where('firstName').equals('STARR'),
 
 // How many people voted in the 2016 general election (GE16)?
-Voter.find().where('historyString').equals(2003),
+Voter.find().where('historyString').in('GE16'),
 
-// Who teaches 362?
+// What is the last-name that comes last in the county in alphabetical order?
 Voter.find().sort('-lastName').limit(1),
 
 // How many zip codes does the county contain?
@@ -30,10 +30,10 @@ Voter.distinct('zipCode')
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(results) {
-    console.log('Number of registered voters that live in zip code 13617 ', results[0]);
-    console.log('The full names of all of the registered voters whose first name is STARR: ', results[1].map(p => p.firstName + p.lastName));
-    console.log('Number of people who voted in the GE 2016: ', results[2]);
-    console.log('last-name that comes in last in the county in alphabetical order:  ', results[3].map(p => p.lastName));
-    console.log('Number of zip codes that the county contains: ', results[4].length);
+    console.log('The number of registered voters that live in zip code 13617: ', results[0].length);
+    console.log('The full names of all of the registered voters whose first-name is STARR: ', results[1].map(p => p.firstName + p.lastName));
+    console.log('Number of people who voted in the GE 2016: ', results[2].length);
+    console.log('The last-name that comes in last in the county in alphabetical order:  ', results[3].map(p => p.lastName));
+    console.log('The number of zip codes that the county contains: ', results[4].length);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
